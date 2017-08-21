1cd6bcc0d7e5dcadde9e8e69a5999de5b56eedcc
Daniel Ayala <imediayala@gmail.com>
2017-08-21 22:14:17 +0200

    ++

---
+++ WeTeamUp/CardPostViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CardPostViewController.swift
+//  WeTeamUp
+//
+//  Created by Daniel Ayala on 21/08/2017.
+//  Copyright © 2017 Daniel Ayala. All rights reserved.
+//
+
+import UIKit
+
+class CardPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
